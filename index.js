e184eda4af49557877721baba25ceadc5d234365
Siyavash Hamdi <siyavash.hamdi@gmail.com>
2021-04-24 02:11:28 +0430

    change pin to 2

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var Gpio = require('onoff').Gpio;   //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'in');        //use GPIO pin 4, and specify that it is output
+const bootstrap = () => {
+    var Gpio = require('onoff').Gpio;   //include onoff to interact with the GPIO
+    var LED = new Gpio(2, 'in');        //use GPIO pin 4, and specify that it is output
 
-setInterval(() => {
-    const res = LED.readSync();
-    console.log(res);
-}, 1000);
+    setInterval(() => {
+        const res = LED.readSync();
+        console.log(res);
+    }, 1000);
+}
+
+
+console.log("Started");
+
+bootstrap();
