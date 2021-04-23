2a4845024811f3ced48c04384cfffd6511220ae6
Siyavash Hamdi <siyavash.hamdi@gmail.com>
2021-04-24 02:54:28 +0430

    Use reminder instead of !

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const bootstrap = () => {
     var Gpio = require('onoff').Gpio;   //include onoff to interact with the GPIO
     var LED2 = new Gpio(2, 'in');
     var LED3 = new Gpio(3, 'out');
-    
+
     let currState = 0;
 
     setInterval(() => {
         const currentDate = new Date().toISOString();
 
-        currState = !currState;
+        currState = (currState + 1) % 2;
         LED3.writeSync(currState);
 
         const led2 = LED2.readSync();
