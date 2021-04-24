2845816362bc98f4609e51eaa6bf1920662aca75
Siyavash Hamdi <siyavash.hamdi@gmail.com>
2021-04-24 23:07:04 +0430

    Added serial-port test

---
+++ serial-port.js
@@ -0,0 +1,25 @@
+const bootstrap = () => {
+    const SerialPort = require("serialport");
+
+    // var portsList = [];
+    SerialPort.list().then((ports) => {
+        ports.forEach((port) => {
+            var portInfo = {
+                portPath: port.path,
+                portManufacturer: port.manufacturer,
+            };
+
+            // portsList.push(portInfo);
+            console.log("Port: ", portInfo);
+        });
+    });
+
+    // console.log(portsList.length);
+
+    setInterval(() => {
+        console.log("live");
+    }, 1000);
+}
+
+console.log("Serial port started");
+bootstrap();
