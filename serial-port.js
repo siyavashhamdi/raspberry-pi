3d22d48586c3e2276ce15bdcb559b5ba1bbf49ed
Siyavash <siyavashhamdi@gmail.com>
2021-04-27 02:49:50 +0430

    Serial port changes

--- serial-port.js
+++ serial-port.js
@@ -2,7 +2,7 @@ const bootstrap = () => {
     const raspi = require('raspi');
     const Serial = require('raspi-serial').Serial;
 
-    const serialPort = new Serial({ baudRate: 57600 });
+    const serialPort = new Serial({ baudRate: 230400 });
 
     serialPort.writeWithCr = (cmd) => {
         serialPort.write(`${ cmd }\r`);
