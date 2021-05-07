0705f71524fba737e679f19083b46ea3d0d530d9
Siyavash <siyavashhamdi@gmail.com>
2021-05-08 01:29:54 +0430

    input/output changes

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ const bootstrap = () => {
             console.log({ SL: 'btn-watch', index, value });
         }
 
-        for (let i = 0; i < pin.output.length; i += 1) {
-            const currPinIndex = pin.output[i];
+        for (let i = 0; i < pin.input.length; i += 1) {
+            const currPinIndex = pin.input[i];
             const currBtn = new Gpio(currPinIndex, 'in', 'rising', { debounceTimeout: 10 });
+            
             currBtn.watch((err, value) => {
                 if (err) {
                     throw err;
