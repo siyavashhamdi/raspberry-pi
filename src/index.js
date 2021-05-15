667447b47054b42267fc8f5e3d7736aa0b8d493b
Siyavash Hamdi <siyavash.hamdi@gmail.com>
2021-05-15 22:38:29 +0430

    Test board

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
 const bootstrap = () => {
     var Gpio = require('onoff').Gpio;   //include onoff to interact with the GPIO
 
-    const pinRig2 = 6;
-    const pinRig1and3 = 13;
-    const pinRigCooler = 5;
-
-    const valRig2 = 0;
-    const valRig1and3 = 0;
-    const valRigCooler = 1;
+    const pin = {
+        rig1and3 = { no: 13, value: 0 },
+        rig2 = { no: 6, value: 0 },
+        cooler = { no: 5, value: 0 },
+    }
 
     setTimeout(() => {
-        const output = new Gpio(pinRig2, 'out');
-        output.writeSync(valRig2);
+        const output = new Gpio(pin.rig2.no, 'out');
+        output.writeSync(pin.rig2.val);
 
         const currentDate = new Date().toISOString();
-        console.log(`[${currentDate}] { currPin: ${pinRig2}, value: ${valRig2} desc: 'pinRig2' }`);
+        console.log(`[${currentDate}] { currPin: ${pin.rig2.no}, value: ${pin.rig2.val} desc: 'pinRig2' }`);
     }, 1000);
 
     setTimeout(() => {
-        const output = new Gpio(pinRig1and3, 'out');
-        output.writeSync(valRig1and3);
+        const output = new Gpio(pin.rig1and3.no, 'out');
+        output.writeSync(pin.rig1and3.val);
 
         const currentDate = new Date().toISOString();
-        console.log(`[${currentDate}] { currPin: ${2}, value: ${valRig1and3}, desc: 'pinRig1and3' }`);
+        console.log(`[${currentDate}] { currPin: ${pin.rig1and3.no}, value: ${pin.rig1and3.val}, desc: 'pinRig1and3' }`);
     }, 2000);
 
     setTimeout(() => {
-        const output = new Gpio(pinRigCooler, 'out');
-        output.writeSync(valRigCooler);
+        const output = new Gpio(pin.cooler.no, 'out');
+        output.writeSync(pin.cooler.val);
 
         const currentDate = new Date().toISOString();
-        console.log(`[${currentDate}] { currPin: ${3}, value: ${valRigCooler}, desc: 'pinRigCooler' }`);
+        console.log(`[${currentDate}] { currPin: ${pin.cooler.no}, value: ${pin.cooler.val}, desc: 'pinRigCooler' }`);
     }, 3000);
 }
 
