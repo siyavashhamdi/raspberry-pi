3a0342855ec2c47d6d46aadbd98d8c5c0066f6b8
Siyavash Hamdi <siyavash.hamdi@gmail.com>
2021-05-15 22:42:57 +0430

    Test board

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const bootstrap = () => {
 
     const pin = {};
 
-    pin.rig1and3 = { no: 13, value: 0 };
-    pin.rig2 = { no: 6, value: 0 };
-    pin.cooler = { no: 5, value: 0 };
+    pin.rig1and3 = { no: 13, value: 1 };
+    pin.rig2 = { no: 6, value: 1 };
+    pin.cooler = { no: 5, value: 1 };
 
     setTimeout(() => {
         const output = new Gpio(pin.rig2.no, 'out');
