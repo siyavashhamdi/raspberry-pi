d12ef3ecd4047a83a3c33ad2f94a0e83960d1a69
Siyavash <siyavashhamdi@gmail.com>
2021-05-20 01:07:53 +0430

    Reset1

--- src/index.js
+++ src/index.js
@@ -17,12 +17,24 @@ const bootstrap = () => {
     // }, 1000);
 
     setTimeout(() => {
+        pin.rig1and3 = { no: 6, val: 0 };
+
         const output = new Gpio(pin.rig1and3.no, 'out');
         output.writeSync(pin.rig1and3.val);
 
         const currentDate = new Date().toISOString();
-        console.log(`[${currentDate}] { currPin: ${pin.rig1and3.no}, value: ${pin.rig1and3.val}, desc: 'pinRig1and3' }`);
-    }, 2000);
+        console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
+    }, 1000);
+
+    setTimeout(() => {
+        pin.rig1and3 = { no: 6, val: 1 };
+
+        const output = new Gpio(pin.rig1and3.no, 'out');
+        output.writeSync(pin.rig1and3.val);
+
+        const currentDate = new Date().toISOString();
+        console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
+    }, 10000);
 
     // setTimeout(() => {
     //     const output = new Gpio(pin.cooler.no, 'out');
