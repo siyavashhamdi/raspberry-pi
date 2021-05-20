ef02504f0d936763b4c4d7ca1701a97c6cefa135
Siyavash <siyavashhamdi@gmail.com>
2021-05-21 03:00:28 +0430

    Unset all

--- src/index.js
+++ src/index.js
@@ -4,21 +4,11 @@ const bootstrap = () => {
     const pin = {};
 
     // val: 1 => ON
-    pin.rig1and3 = { no: 6, val: 1 };
-    pin.rig2 = { no: 5, val: 1 };
-    pin.cooler = { no: 13, val: 1 };
-
-    // setTimeout(() => {
-    //     const output = new Gpio(pin.rig2.no, 'out');
-    //     output.writeSync(pin.rig2.val);
-
-    //     const currentDate = new Date().toISOString();
-    //     console.log(`[${currentDate}] { currPin: ${pin.rig2.no}, value: ${pin.rig2.val} desc: 'pinRig2' }`);
-    // }, 1000);
+    pin.rig1and3 = { no: 6, val: 0 };
+    pin.rig2 = { no: 5, val: 0 };
+    pin.cooler = { no: 13, val: 0 };
 
     setTimeout(() => {
-        pin.rig1and3 = { no: 6, val: 0 };
-
         const output = new Gpio(pin.rig1and3.no, 'out');
         output.writeSync(pin.rig1and3.val);
 
@@ -27,24 +17,20 @@ const bootstrap = () => {
     }, 1000);
 
     setTimeout(() => {
-        pin.rig1and3 = { no: 6, val: 1 };
-
-        const output = new Gpio(pin.rig1and3.no, 'out');
-        output.writeSync(pin.rig1and3.val);
+        const output = new Gpio(pin.rig2.no, 'out');
+        output.writeSync(pin.rig2.val);
 
         const currentDate = new Date().toISOString();
-        console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
-    }, 10000);
+        console.log(`[${ currentDate }] { currPin: ${ pin.rig2.no }, value: ${ pin.rig2.val } desc: 'pinRig2' }`);
+    }, 1000);
 
-    // setTimeout(() => {
-    //     pin.cooler = { no: 13, val: 0 };
+    setTimeout(() => {
+        const output = new Gpio(pin.cooler.no, 'out');
+        output.writeSync(pin.cooler.val);
 
-    //     const output = new Gpio(pin.cooler.no, 'out');
-    //     output.writeSync(pin.cooler.val);
-
-    //     const currentDate = new Date().toISOString();
-    //     console.log(`[${currentDate}] { currPin: ${pin.cooler.no}, value: ${pin.cooler.val}, desc: 'pinRigCooler' }`);
-    // }, 15000);
+        const currentDate = new Date().toISOString();
+        console.log(`[${ currentDate }] { currPin: ${ pin.cooler.no }, value: ${ pin.cooler.val }, desc: 'pinRigCooler' }`);
+    }, 15000);
 }
 
 console.log("Started");
