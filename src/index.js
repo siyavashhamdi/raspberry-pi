b94fea56fd3cb02a0f642047cf64cb0009cc0f6b
Siyavash <siyavashhamdi@gmail.com>
2021-05-20 19:49:06 +0430

    x

--- src/index.js
+++ src/index.js
@@ -17,16 +17,6 @@ const bootstrap = () => {
     // }, 1000);
 
     setTimeout(() => {
-        pin.rig1and3 = { no: 6, val: 0 };
-
-        const output = new Gpio(pin.rig1and3.no, 'out');
-        output.writeSync(pin.rig1and3.val);
-
-        const currentDate = new Date().toISOString();
-        console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
-    }, 1000);
-
-    setTimeout(() => {
         pin.rig1and3 = { no: 6, val: 1 };
 
         const output = new Gpio(pin.rig1and3.no, 'out');
@@ -34,17 +24,27 @@ const bootstrap = () => {
 
         const currentDate = new Date().toISOString();
         console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
-    }, 10000);
+    }, 2000);
 
-    setTimeout(() => {
-        pin.cooler = { no: 13, val: 0 };
+    // setTimeout(() => {
+    //     pin.rig1and3 = { no: 6, val: 1 };
 
-        const output = new Gpio(pin.cooler.no, 'out');
-        output.writeSync(pin.cooler.val);
+    //     const output = new Gpio(pin.rig1and3.no, 'out');
+    //     output.writeSync(pin.rig1and3.val);
 
-        const currentDate = new Date().toISOString();
-        console.log(`[${currentDate}] { currPin: ${pin.cooler.no}, value: ${pin.cooler.val}, desc: 'pinRigCooler' }`);
-    }, 15000);
+    //     const currentDate = new Date().toISOString();
+    //     console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
+    // }, 10000);
+
+    // setTimeout(() => {
+    //     pin.cooler = { no: 13, val: 0 };
+
+    //     const output = new Gpio(pin.cooler.no, 'out');
+    //     output.writeSync(pin.cooler.val);
+
+    //     const currentDate = new Date().toISOString();
+    //     console.log(`[${currentDate}] { currPin: ${pin.cooler.no}, value: ${pin.cooler.val}, desc: 'pinRigCooler' }`);
+    // }, 15000);
 }
 
 console.log("Started");
