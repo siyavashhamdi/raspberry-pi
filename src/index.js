0f78a57095488cd94442f21668bb2faa62289e3f
Siyavash <siyavashhamdi@gmail.com>
2021-05-21 03:03:56 +0430

    Set all

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const bootstrap = () => {
     const pin = {};
 
     // val: 1 => ON
-    pin.rig1and3 = { no: 6, val: 0 };
-    pin.rig2 = { no: 5, val: 0 };
+    pin.rig1and3 = { no: 6, val: 1 };
+    pin.rig2 = { no: 5, val: 1 };
     pin.cooler = { no: 13, val: 0 };
 
     setTimeout(() => {
@@ -24,13 +24,13 @@ const bootstrap = () => {
         console.log(`[${ currentDate }] { currPin: ${ pin.rig2.no }, value: ${ pin.rig2.val } desc: 'pinRig2' }`);
     }, 5000);
 
-    setTimeout(() => {
-        const output = new Gpio(pin.cooler.no, 'out');
-        output.writeSync(pin.cooler.val);
+    // setTimeout(() => {
+    //     const output = new Gpio(pin.cooler.no, 'out');
+    //     output.writeSync(pin.cooler.val);
 
-        const currentDate = new Date().toISOString();
-        console.log(`[${ currentDate }] { currPin: ${ pin.cooler.no }, value: ${ pin.cooler.val }, desc: 'pinRigCooler' }`);
-    }, 10000);
+    //     const currentDate = new Date().toISOString();
+    //     console.log(`[${ currentDate }] { currPin: ${ pin.cooler.no }, value: ${ pin.cooler.val }, desc: 'pinRigCooler' }`);
+    // }, 10000);
 }
 
 console.log("Started");
