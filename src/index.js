38c39b34c4019a6ce9cdf65c9a9cd0ca795638b3
Siyavash <siyavashhamdi@gmail.com>
2021-05-20 19:51:07 +0430

    x

--- src/index.js
+++ src/index.js
@@ -17,6 +17,16 @@ const bootstrap = () => {
     // }, 1000);
 
     setTimeout(() => {
+        pin.rig1and3 = { no: 6, val: 0 };
+
+        const output = new Gpio(pin.rig1and3.no, 'out');
+        output.writeSync(pin.rig1and3.val);
+
+        const currentDate = new Date().toISOString();
+        console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
+    }, 1000);
+
+    setTimeout(() => {
         pin.rig1and3 = { no: 6, val: 1 };
 
         const output = new Gpio(pin.rig1and3.no, 'out');
@@ -24,17 +34,7 @@ const bootstrap = () => {
 
         const currentDate = new Date().toISOString();
         console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
-    }, 2000);
-
-    // setTimeout(() => {
-    //     pin.rig1and3 = { no: 6, val: 1 };
-
-    //     const output = new Gpio(pin.rig1and3.no, 'out');
-    //     output.writeSync(pin.rig1and3.val);
-
-    //     const currentDate = new Date().toISOString();
-    //     console.log(`[${ currentDate }] { currPin: ${ pin.rig1and3.no }, value: ${ pin.rig1and3.val }, desc: 'pinRig1and3' }`);
-    // }, 10000);
+    }, 10000);
 
     // setTimeout(() => {
     //     pin.cooler = { no: 13, val: 0 };
