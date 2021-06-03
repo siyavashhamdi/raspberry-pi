626a1156f7ead4ed4110a9081720e3a3ac6457c7
Siyavash <siyavashhamdi@gmail.com>
2021-06-04 00:56:13 +0430

    Reset rig1-3

--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ const bootstrap = () => {
     const pin = {};
 
     // val: 1 => ON
-    pin.rig1and3 = { no: 6, val: 1 };
+    pin.rig1and3 = { no: 6, val: 0 };
     pin.rig2 = { no: 5, val: 1 };
     pin.cooler = { no: 13, val: 0 };
 
-    // setTimeout(() => {
-    //     const output = new Gpio(pin.rig2.no, 'out');
-    //     output.writeSync(pin.rig2.val);
+    setTimeout(() => {
+        const output = new Gpio(pin.rig2.no, 'out');
+        output.writeSync(pin.rig2.val);
 
-    //     const currentDate = new Date().toISOString();
-    //     console.log(`[${currentDate}] { currPin: ${pin.rig2.no}, value: ${pin.rig2.val} desc: 'pinRig2' }`);
-    // }, 1000);
+        const currentDate = new Date().toISOString();
+        console.log(`[${currentDate}] { currPin: ${pin.rig2.no}, value: ${pin.rig2.val} desc: 'pinRig2' }`);
+    }, 1000);
+
+    setTimeout(() => {
+        pin.cooler.val = 1;
+
+        const output = new Gpio(pin.rig2.no, 'out');
+        output.writeSync(pin.rig2.val);
+
+        const currentDate = new Date().toISOString();
+        console.log(`[${currentDate}] { currPin: ${pin.rig2.no}, value: ${pin.rig2.val} desc: 'pinRig2' }`);
+    }, 10000);
 
     // setTimeout(() => {
     //     const output = new Gpio(pin.rig1and3.no, 'out');
@@ -24,23 +34,23 @@ const bootstrap = () => {
     //     console.log(`[${currentDate}] { currPin: ${pin.rig1and3.no}, value: ${pin.rig1and3.val}, desc: 'pinRig1and3' }`);
     // }, 2000);
 
-    setTimeout(() => {
-        const output = new Gpio(pin.cooler.no, 'out');
-        output.writeSync(pin.cooler.val);
+    // setTimeout(() => {
+    //     const output = new Gpio(pin.cooler.no, 'out');
+    //     output.writeSync(pin.cooler.val);
 
-        const currentDate = new Date().toISOString();
-        console.log(`[${currentDate}] { currPin: ${pin.cooler.no}, value: ${pin.cooler.val}, desc: 'pinRigCooler' }`);
-    }, 3000);
+    //     const currentDate = new Date().toISOString();
+    //     console.log(`[${currentDate}] { currPin: ${pin.cooler.no}, value: ${pin.cooler.val}, desc: 'pinRigCooler' }`);
+    // }, 3000);
 
-    setTimeout(() => {
-        pin.cooler.val = 1;
+    // setTimeout(() => {
+    //     pin.cooler.val = 1;
         
-        const output = new Gpio(pin.cooler.no, 'out');
-        output.writeSync(pin.cooler.val);
+    //     const output = new Gpio(pin.cooler.no, 'out');
+    //     output.writeSync(pin.cooler.val);
 
-        const currentDate = new Date().toISOString();
-        console.log(`[${currentDate}] { currPin: ${pin.cooler.no}, value: ${pin.cooler.val}, desc: 'pinRigCooler' }`);
-    }, 6000);
+    //     const currentDate = new Date().toISOString();
+    //     console.log(`[${currentDate}] { currPin: ${pin.cooler.no}, value: ${pin.cooler.val}, desc: 'pinRigCooler' }`);
+    // }, 6000);
 }
 
 console.log("Started");
