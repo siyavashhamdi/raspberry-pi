15e8448db16a81dbb53c9f1edafc12bf14460da6
Siyavash Hamdi <siyavash.hamdi@gmail.com>
2021-06-30 00:03:12 +0430

    pin 11 as output test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const bootstrap = () => {
     var Gpio = require('onoff').Gpio;   //include onoff to interact with the GPIO
     // 11, 19, 26, 21, 20
 
-    const output = new Gpio(19, 'out');
+    const output = new Gpio(26, 'out');
     output.writeSync(0);
 }
 
