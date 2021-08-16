9f5bb28ad821d0b5d563825a10774f4e8f843f29
Siyavash <siyavashhamdi@gmail.com>
2021-08-16 13:43:37 +0430

    Added fileLog

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -1,4 +1,5 @@
 import * as moment from 'moment-timezone';
+import { appendFileSync, existsSync, mkdirSync } from 'fs';
 
 export class Utils {
   public static zeroPad = (num: unknown, places: number): string => String(num).padStart(places, '0');
@@ -16,23 +17,45 @@ export class Utils {
     return addedDate.toDate();
   }
 
+    public static convertKeyVal2Obj(keyVal: Array<string>): any {
+        const objKeyVal = {};
+
+        for (const arg of keyVal) {
+            const [key, val] = arg.substring(2).split('=');
+
+            Object.assign(objKeyVal, { [key]: val });
+        }
+
+        return objKeyVal;
+    }
+
   public static consoleLog(logValue: string | number): void {
     const dateTime = this.formatDateTime(new Date());
     const modifiedLogValue = `[${ dateTime }] : ${ logValue }`;
 
     // eslint-disable-next-line no-console
     console.log(modifiedLogValue);
+
+      Utils.fileLog(modifiedLogValue);
   }
 
-  public static convertKeyVal2Obj(keyVal: Array<string>): any {
-    const objKeyVal = {};
+    public static async fileLog(logValue: string | number): Promise<void> {
+        let logDirPath = require?.main?.path;
 
-    for (const arg of keyVal) {
-      const [key, val] = arg.substring(2).split('=');
+        if (!logDirPath) {
+            return;
+        }
 
-      Object.assign(objKeyVal, { [key]: val });
-    }
+      logDirPath += '/log/';
 
-    return objKeyVal;
+      if (!existsSync(logDirPath)) {
+          mkdirSync(logDirPath);
+      }
+
+      const todayDate = moment(new Date()).tz('Asia/Tehran').format('yyyyMMDD');
+      const logFileName = `${ todayDate }.log`;
+      const logFullPath = logDirPath + logFileName;
+
+      appendFileSync(logFullPath, `${ logValue.toString() }\n`);
   }
 }
