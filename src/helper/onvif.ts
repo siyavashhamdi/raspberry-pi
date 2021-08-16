d6a841734fb9a1966702945115e93a2f31184bf4
Siyavash <siyavashhamdi@gmail.com>
2021-08-17 03:45:31 +0430

    Added test node-onvif

--- src/helper/onvif.ts
+++ src/helper/onvif.ts
@@ -25,9 +25,9 @@ export class Onvif {
   public static takeSnapshot() {
     // Create an OnvifDevice object
     const device = new onvif.OnvifDevice({
-      xaddr: '192.168.1.162',
+      xaddr: 'http://192.168.1.162/onvif/device_service',
       user: 'admin',
-      pass: '2690fF2690',
+      pass: '0000',
     });
 
     // Initialize the OnvifDevice object
