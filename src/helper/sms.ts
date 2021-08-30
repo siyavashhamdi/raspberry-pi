417108b56cd3e0a7c16a07ad17848f732174c253
Siyavash <siyavashhamdi@gmail.com>
2021-08-30 13:32:22 +0430

    Subscribe message received as test

--- src/helper/sms.ts
+++ src/helper/sms.ts
@@ -89,6 +89,7 @@ export class SMS {
 
     // 115200 | 57600 | 38400 | 19200 | 9600 | 4800 | 2400 | 1800 | 1200 | 600 | 300 | 200 | 150 | 134 | 110 | 75 | 50 | number
     await Utils.sleep(5000);
+    Utils.consoleLog('Setting baudrate');
     this.setBaudRate(115200);
   }
 
